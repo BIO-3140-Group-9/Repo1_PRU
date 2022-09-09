077dc0461e21adb6ff0feb5dcd47a1a9c591667e
James Kim <jameskim@macbook-pro-53.bc.edu>
2022-09-09 10:27:16 -0400

    09-09-2022 Commit

---
+++ Module1_Repo1.R
@@ -0,0 +1,36 @@
+library(ggplot2)
+library(tidyverse)
+
+dat <- read.csv("scales.csv")
+
+sapply(dat,class)
+
+dim(dat)
+
+species.n <- dat %>% 
+  group_by(species) %>%
+  summarise(n.puncture = n())
+
+species.s <- dat %>%
+  count(species,specimen) %>%
+  print() %>%
+  count(species,name = "n.specimens")
+
+data_summary <- species.n %>%
+  mutate(species.s)
+species.ns
+
+view(data_summary)
+#complies the code to show one table with n.puncture and n.specimen
+
+pdf("species.quadrant.pdf")
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+dev.off()
+
+list.files(pattern=".pdf")
+
