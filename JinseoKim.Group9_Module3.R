beeb450b2f14aec1b9c441a3d844edd9f6948fb1
Clementine Paris <clem@clems-air.bc.edu>
2022-09-26 22:50:57 +0200

    26/09/22

---
+++ JinseoKim.Group9_Module3.R
@@ -0,0 +1,50 @@
+library(tidyverse) 
+library(ape)
+library(nlme)
+library(geiger)
+library(caper)
+library(phytools)
+library(viridis)
+library(MuMIn)
+
+anole <- read_csv("anole.dat.csv")
+anole.eco <- read_csv("anole.eco.csv")
+
+anole2 <- anole%>%
+  left_join(anole.eco)%>%
+  filter(!Ecomorph%in%c("U","CH"))%>%
+  na.omit()
+
+anole.log <- anole2%>%
+  mutate_at(c("SVL", "HTotal","PH","ArbPD"),log)%>%
+  print ()
+
+anole.log.PH.lm <- lm(HTotal~SVL+PH,anole.log)
+anole.log.ArbPD.lm <- lm(HTotal~SVL+ArbPD,anole.log)
+
+anole.log%>%
+  ggplot(aes(SVL,HTotal))+geom_point()+geom_abline(slope=coef(anole.log.PH.lm)[2],intercept=coef(anole.log.PH.lm)[1],col="blue")
+
+anole.log%>%
+  ggplot(aes(SVL,HTotal))+geom_point()+geom_abline(slope=coef(anole.log.ArbPD.lm)[2],intercept=coef(anole.log.ArbPD.lm)[1],col="blue")
+
+anole.log.res <-anole.log%>%
+  mutate(res=residuals(anole.log.PH.lm))%>%
+  mutate(res=residuals(anole.log.ArbPD.lm))%>%
+  print()
+
+#lol no idea how to do three
+
+anole.tree <- read.tree("anole.tre")
+
+pgls.BM1.PH <- gls(HTotal ~SVL+PH, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+pgls.BM2.ArbPD <- gls(HTotal ~SVL+ArbPD, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+pgls.BM3.Both <- gls(HTotal ~SVL+PH+ArbPD, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+anole.phylo.aic <- AICc(pgls.BM1.PH,pgls.BM2.ArbPD,pgls.BM3.Both)
+aicw(anole.phylo.aic$AICc)
+#According to the AIC test, both PH and ArbPD together are significant predictor
+
+anova(pgls.BM3.Both)
