c5dbd255ec0f80566fbcb7acc548858335a9bd0e
Clementine Paris <clem@clems-air.bc.edu>
2022-09-26 22:52:35 +0200

    26/09/22

--- JinseoKim.Group9_Module3.R
+++ JinseoKim.Group9_Module3.R
@@ -7,6 +7,10 @@ library(phytools)
 library(viridis)
 library(MuMIn)
 
+<<<<<<< HEAD
+=======
+#1 establishing the anole.log tibble
+>>>>>>> 6686565c36605e17e3299288078ff126dfe53b3e
 anole <- read_csv("anole.dat.csv")
 anole.eco <- read_csv("anole.eco.csv")
 
@@ -19,10 +23,15 @@ anole.log <- anole2%>%
   mutate_at(c("SVL", "HTotal","PH","ArbPD"),log)%>%
   print ()
 
+<<<<<<< HEAD
+=======
+#2 Plotting two simple linear model
+>>>>>>> 6686565c36605e17e3299288078ff126dfe53b3e
 anole.log.PH.lm <- lm(HTotal~SVL+PH,anole.log)
 anole.log.ArbPD.lm <- lm(HTotal~SVL+ArbPD,anole.log)
 
 anole.log%>%
+<<<<<<< HEAD
   ggplot(aes(SVL,HTotal))+geom_point()+geom_abline(slope=coef(anole.log.PH.lm)[2],intercept=coef(anole.log.PH.lm)[1],col="blue")
 
 anole.log%>%
@@ -35,6 +44,25 @@ anole.log.res <-anole.log%>%
 
 #lol no idea how to do three
 
+=======
+  ggplot(aes(SVL,HTotal))+geom_point()+geom_abline(slope=coef(anole.log.PH.lm)[2],intercept=coef(anole.log.PH.lm)[1],col="red")
+
+anole.log%>%
+  ggplot(aes(SVL,HTotal))+geom_point()+geom_abline(slope=coef(anole.log.ArbPD.lm)[2],intercept=coef(anole.log.ArbPD.lm)[1],col="green")
+
+#3 Plotting res against PH and ArbPD
+anole.log.res <-anole.log%>%
+  mutate(res.PH =residuals(anole.log.PH.lm))%>%
+  mutate(res.ArbPD =residuals(anole.log.ArbPD.lm))%>%
+  print()
+
+anole.log.res%>%
+  ggplot(aes(x=Ecomorph,y=res.PH, col=Ecomorph)) +geom_boxplot()+stat_summary(fun=mean, geom="point", size=3)
+anole.log.res%>%
+  ggplot(aes(x=Ecomorph,y=res.ArbPD, col=Ecomorph)) +geom_boxplot()+stat_summary(fun=mean, geom="point", size=3)
+
+#4 Constructing PGLS model
+>>>>>>> 6686565c36605e17e3299288078ff126dfe53b3e
 anole.tree <- read.tree("anole.tre")
 
 pgls.BM1.PH <- gls(HTotal ~SVL+PH, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
@@ -43,8 +71,19 @@ pgls.BM2.ArbPD <- gls(HTotal ~SVL+ArbPD, correlation = corBrownian(1,phy = anole
 
 pgls.BM3.Both <- gls(HTotal ~SVL+PH+ArbPD, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
 
+<<<<<<< HEAD
+=======
+#5 Analyzing PGLS model using AIC analysis
+>>>>>>> 6686565c36605e17e3299288078ff126dfe53b3e
 anole.phylo.aic <- AICc(pgls.BM1.PH,pgls.BM2.ArbPD,pgls.BM3.Both)
 aicw(anole.phylo.aic$AICc)
 #According to the AIC test, both PH and ArbPD together are significant predictor
 
 anova(pgls.BM3.Both)
+<<<<<<< HEAD
+=======
+
+#6 
+
+
+>>>>>>> 6686565c36605e17e3299288078ff126dfe53b3e
