63e1ddae6f9c8893f91b3d23fd48868b65cbb68d
Christopher Kenaley <cpkenaley@gmail.com>
2022-10-13 09:15:27 -0400

    proj3/4 feedback

---
+++ Group9_Module3_FinalDraft_Comments.R
@@ -0,0 +1,74 @@
+library(tidyverse) 
+library(ape)
+library(nlme)
+library(geiger)
+library(caper)
+library(phytools)
+library(viridis)
+library(MuMIn)
+
+#1 establishing the anole.log tibble
+anole <- read_csv("anole.dat.csv")
+anole.eco <- read_csv("anole.eco.csv")
+
+anole2 <- anole%>%
+  left_join(anole.eco)%>%
+  filter(!Ecomorph%in%c("U","CH"))%>%
+  na.omit()
+
+anole.log <- anole2%>%
+  mutate_at(c("SVL", "HTotal","PH","ArbPD"),log)%>%
+  print ()
+
+#2 Plotting two simple linear model
+anole.log.PH.lm <- lm(HTotal~SVL+PH,anole.log)
+anole.log.ArbPD.lm <- lm(HTotal~SVL+ArbPD,anole.log)
+
+anole.log%>%
+  ggplot(aes(SVL,HTotal))+geom_point()+geom_abline(slope=coef(anole.log.PH.lm)[2],intercept=coef(anole.log.PH.lm)[1],col="red")
+
+anole.log%>%
+  ggplot(aes(SVL,HTotal))+geom_point()+geom_abline(slope=coef(anole.log.ArbPD.lm)[2],intercept=coef(anole.log.ArbPD.lm)[1],col="green")
+
+#3 Plotting res against PH and ArbPD
+anole.log.res <-anole.log%>%
+  mutate(res.PH =residuals(anole.log.PH.lm))%>%
+  mutate(res.ArbPD =residuals(anole.log.ArbPD.lm))%>%
+  print()
+
+anole.log.res%>%
+  ggplot(aes(x=Ecomorph,y=res.PH, col=Ecomorph)) +geom_boxplot()+stat_summary(fun=mean, geom="point", size=3)
+anole.log.res%>%
+  ggplot(aes(x=Ecomorph,y=res.ArbPD, col=Ecomorph)) +geom_boxplot()+stat_summary(fun=mean, geom="point", size=3)
+
+#4 Constructing PGLS model
+anole.tree <- read.tree("anole.tre")
+
+pgls.BM1.PH <- gls(HTotal ~SVL+PH, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+pgls.BM2.ArbPD <- gls(HTotal ~SVL+ArbPD, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+pgls.BM3.Both <- gls(HTotal ~SVL+PH+ArbPD, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+#5 Analyzing PGLS model using AIC analysis
+anole.phylo.aic <- AICc(pgls.BM1.PH,pgls.BM2.ArbPD,pgls.BM3.Both)
+aicw(anole.phylo.aic$AICc)
+#According to the AIC test, both PH and ArbPD together are significant predictor
+
+anova(pgls.BM3.Both)
+
+#6 Visualizing Effect of PD, ArbPD, or Both on the SVl~Hindlimb Residuals
+anole.log <- anole.log%>%
+  mutate(phylo.res.PH=residuals(pgls.BM1.PH))%>%
+  mutate(phylo.res.ArbPD=residuals(pgls.BM2.ArbPD))%>%
+  mutate(phylo.res.Both=residuals(pgls.BM3.Both))%>%
+  print()
+
+anole.log%>%
+  dplyr::select(Ecomorph,phylo.res.PH,phylo.res.ArbPD,phylo.res.Both)%>%
+  pivot_longer(cols=c("phylo.res.PH","phylo.res.ArbPD","phylo.res.Both"))%>%
+  print%>%
+  ggplot(aes(x=Ecomorph,y=value, col=Ecomorph)) +geom_boxplot() +stat_summary(fun=mean, geom="point", size=3)+facet_grid(name~.,scales = "free_y")+ylab("residual")
+
+
+#CPK: Excellent work! Really nice analysis and code that produces pleasing and explanatory figures! Well done! 15/15
